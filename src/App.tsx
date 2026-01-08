26b0cf66d8ee775df22545ffa182d0641da67c41
Tony Okeke <tonykabilanokeke@gmail.com>
2026-01-07 21:37:49 -0500

    set up convex and vercel analytics

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,14 @@
 import { ComponentExample } from "@/components/component-example";
+import { Analytics } from "@vercel/analytics/next";
 
 export function App() {
-return <ComponentExample />;
+  return (
+    <>
+      <ComponentExample />
+      <Analytics />
+    </>
+  );
 }
 
 export default App;
+
